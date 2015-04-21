dbddc9be332373318a143e2032fd1bc1ef072c54
Your Name <you@example.com>
2015-04-21 15:11:33 +0200

    first commit

--- archlinux_scripts/geeknote_edit.sh
+++ archlinux_scripts/geeknote_edit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 FILENAME=$1
 NOTE_NAME=`echo $FILENAME | sed 's/.*\///'`
-NOTE_PATH=`echo $FILENAME | sed 's/'$NOTE_NAME'//'`
+NOTE_PATH=`echo $FILENAME | sed 's/'"$NOTE_NAME"'//'`
 LOG_PATH=''
 
 #echo -e "pwd `pwd`\nfilename $FILENAME\nnote_path $NOTE_PATH\nnote_name $NOTE_NAME"
 
-gnsync --path $NOTE_PATH --mask $NOTE_NAME $2 $3 
+gnsync --path $NOTE_PATH --mask "$NOTE_NAME" $2 $3 
 #--logpath $LOG_PATH
 
 
